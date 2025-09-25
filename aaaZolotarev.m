dbef05ddc3f3157b735e4b3b6ca3261da48e0093
Marcus Webb <marcusdavidwebb@gmail.com>
2025-09-25 13:24:13 +0100

    Add comments to aaaZolotarev function
    Added comments to describe the function's purpose and requirements.

--- aaaZolotarev.m
+++ aaaZolotarev.m
@@ -1,4 +1,8 @@
 function [roots, poles, sigma] = aaaZolotarev(E,F,n)
+% approximates the roots and poles of the Zolotarev rational function for Z_n(E,F)
+% and returns sigma = Z_n(E,F)
+% requires Chebfun
+
 fEF = [-ones(length(E),1); ones(length(F),1)];
 [q,~,~,~,zj,fj,weights] = aaa(fEF,[E;F],'degree',n,'sign',1,'lawson',300,'damping',0.9);
 tau = norm(fEF-q([E;F]),inf);
